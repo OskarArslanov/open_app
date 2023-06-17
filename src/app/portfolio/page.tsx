67f016ade60e11d4543abd2f72af03e2b57e4ea5
oskar <oskararslanov@yandex.ru>
2023-06-17 19:06:12 +0600

    add charts to platforms

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,22 +1,7 @@
 import styles from './portfolio.module.css';
 
 const PortfolioPage = () => {
-  return (
-    <div className={styles.portfolio}>
-      <div className={styles.portfolio_analytics}>
-        <span className={styles.portfolio_analytics_title}>
-          Вся аналитика в одном кабинете
-        </span>
-        <span className={styles.portfolio_analytics_description}>
-          Отслеживайте работу голосового ассистента в личном кабинете
-        </span>
-      </div>
-      <span className={styles.portfolio_widgets}>
-        Уникальные виджеты позволяют настроить дашборд под задачи различных
-        подразделений вашей компании
-      </span>
-    </div>
-  );
+  return <div>Portfolio</div>;
 };
 
 export default PortfolioPage;
