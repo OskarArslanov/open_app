12028a977f8126a164b0829c04ffa3808ad1d572
oskar <oskararslanov@yandex.ru>
2023-06-19 01:23:35 +0600

    refactored to fsd

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+const repo = 'oskararslanov.github.io';
+let assetPrefix = ``;
+let basePath = `/`;
+
+if (isGithubActions) {
+    // trim off `<owner>/`
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+  }
+
+const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
+
+module.exports = nextConfig;
