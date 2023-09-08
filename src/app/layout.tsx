66ef499286c5b66db488acc13f6c1232498c4c1d
oskar <oskararslanov@yandex.ru>
2023-09-08 11:59:07 +0600

    fixed height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import app from './app.module.css';
 import { FC, PropsWithChildren } from 'react';
 import App from '@/pages/App';
 import { Metadata } from 'next';
+import styled from '@emotion/styled';
 
 const inter = Noto_Sans({
   subsets: ['latin'],
@@ -17,8 +18,8 @@ export const metadata: Metadata = {
 
 const RootLayout: FC<PropsWithChildren> = (props) => {
   return (
-    <html>
-      <body className={`${inter.className} ${app.layout}`}>
+    <html style={{ height: '100%' }}>
+      <body className={inter.className} style={{ height: '100%' }}>
         <App>{props.children}</App>
       </body>
     </html>
